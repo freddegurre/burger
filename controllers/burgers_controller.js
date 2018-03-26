a59ce75a239fd3d152a0547e6183fa5e24148824
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-25 22:18:30 -0700

    all functionality works now only testing and heroku left

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,28 +6,38 @@ var router = express.Router();
 var burgers = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-    burgers.all(function(data) {
-    
-      console.log(data);
-      res.render("index", {burgers: data});
-    });
+router.get("/", function (req, res) {
+  burgers.all(function (data) {
+
+    //sort al burgers here before sending to frontend
+    var devoured = [];
+    var burger = [];
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].devoured === 1) {
+        devoured.push(data[i]);
+      }
+      else {
+        burger.push(data[i]);
+      }
+    }
+    res.render("index", { burgers: burger, eaten: devoured });
+  });
+});
+
+router.post("/api/burger", function (req, res) {
+  burgers.insertOne(req.body.name, function (result) {
+    res.json({ id: result.insertId });
+  });
+  console.log(req.body);
+
+});
+router.put("/api/burger", function (req, res) {
+  console.log(req.body.id);
+
+  burgers.update(req.body.id, function (result) {
+    res.end();
   });
 
-router.post("/api/burger", function(req, res){
- burgers.insertOne(req.body.name, function(result){
-  res.json({ id: result.insertId });
- }); 
-  console.log(req.body); 
-
-}); 
-router.put("/api/burger", function(req, res){
-  console.log(req.body.id); 
-   
-  burgers.update(req.body.id, function(result){
-  res.end();
-  });
-  
 });
 
 
